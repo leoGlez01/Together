3d8dbd3ccf6322458ae3d74ba5104373354ae9f6
leoGlez01 <lworkgonzalez01@gmail.com>
2024-01-03 23:36:31 +0100

    Adding location port to ws route

--- chat/routing.py
+++ chat/routing.py
@@ -1,11 +1,6 @@
 from django.urls import path, include
 from .consumers import ChatConsumer
-from channels.routing import ProtocolTypeRouter, URLRouter
 
 websocket_urlpatterns = [
     path('', ChatConsumer.as_asgi()),
 ]
-
-application = ProtocolTypeRouter({
-    "websocket": URLRouter(websocket_urlpatterns),
-})
