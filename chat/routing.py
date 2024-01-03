235e7218c4b7f4e5f1cebb8827724c7b0d809a40
leoGlez01 <lworkgonzalez01@gmail.com>
2024-01-03 23:06:44 +0100

    Adding token to ws route

--- chat/routing.py
+++ chat/routing.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
 from .consumers import ChatConsumer
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 websocket_urlpatterns = [
     path('', ChatConsumer.as_asgi()),
 ]
+
+application = ProtocolTypeRouter({
+    "websocket": URLRouter(websocket_urlpatterns),
+})
