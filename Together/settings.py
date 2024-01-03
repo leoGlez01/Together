097ff5e593bb2fcb4d295f024d80223754f018af
leoGlez01 <lworkgonzalez01@gmail.com>
2024-01-03 21:34:14 +0100

    Adding CORS ALLOWED ORIGIN

--- Together/settings.py
+++ Together/settings.py
@@ -137,3 +137,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOW_ALL_ORIGINS = True
